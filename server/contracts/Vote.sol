4af1b9129f0cdddd0f7d85fbc54b972e71509cb8
teovlt <teo.villet2@gmail.com>
2025-03-18 08:58:52 +0100

    delete non used prettier configs and add a editorConfig file

--- server/contracts/Vote.sol
+++ server/contracts/Vote.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
-import 'hardhat/console.sol';
+import "hardhat/console.sol";
 struct Voter {
     bool isRegistered;
     bool hasVoted;
