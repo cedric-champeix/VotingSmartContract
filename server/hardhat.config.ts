dfa4148e9bd68d1a6cf61d7ead6301bffc240e9e
teovlt <teo.villet2@gmail.com>
2025-03-18 11:20:25 +0100

    Move .env.example into server

--- server/hardhat.config.ts
+++ server/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {config as dotenvConfig} from "dotenv";
+dotenvConfig();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
