9626287014ac7b9016cbd85ec93fe2e20d4c3f4d
teovlt <teo.villet2@gmail.com>
2025-03-19 09:25:28 +0100

    Fix setup for uni tests again

--- server/hardhat.config.cjs
+++ server/hardhat.config.cjs
@@ -1,13 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import {config as dotenvConfig} from "dotenv";
+const { config: dotenvConfig } = require('dotenv');
 dotenvConfig();
+require("@nomicfoundation/hardhat-toolbox");
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
-const config: HardhatUserConfig = {
+module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
@@ -37,5 +36,3 @@ const config: HardhatUserConfig = {
     enabled: true,
   },
 };
-
-export default config;
